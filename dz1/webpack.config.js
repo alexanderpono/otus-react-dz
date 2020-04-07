82d1a2224c2a29cb8f91ef9ad801c083dd0d6514
Alexander Ponomarenko <aponomarenko@mtt.ru>
2020-04-07 21:02:49 +0300

    dz1	1) create package.json  2) add webpack + configure webpack dev server

---
+++ dz1/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+    plugins: [],
+    entry: './src/app.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'www')
+    }
+    ,
+    devServer: {
+        contentBase: "./www",
+        open: true,
+        watchContentBase: true,
+        port: 8081
+    },
+    module: {
+        rules: [
+        ]
+    }
+}
+
+
