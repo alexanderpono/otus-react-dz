20bc8d10886a3a5ce68b410af487a2e767f702b8
Alexander Ponomarenko <aaponomarenko@vtb.ru>
2020-04-28 21:43:52 +0300

    [dz3]	initial commit. set up webpack, html loader, init babel 	loader

---
+++ dz3/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+    entry: "./src/index.tsx",
+    devtool: "source-map",
+    resolve: {
+        extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+        alias: {
+            types: path.resolve(__dirname, "src/types"),
+        },
+    },
+    output: {
+        path: path.join(__dirname, "/dist"),
+        filename: "index.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(js|ts|tsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            }
+        ],
+    },
+    devServer: {
+        historyApiFallback: true,
+        // contentBase: '../webapp',
+        open: false,
+        port: 8800
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
+        }),
+    ],
+};
